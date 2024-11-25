b7f661dfad984036e44762106baba6ac07d56bfb
hyoseongLee <hyoseong123@naver.com>
2024-11-25 18:21:37 +0900

    2024.11.25

--- src/component/ProductAll.js
+++ src/component/ProductAll.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../Navibar/ProductCard';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([])
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
-    <div> 상품 페이지 </div>
+    <div>
+      <Container>
+        <Row>
+          {productList.map((menu) => (
+            <Col lg={3}> <ProductCard item={menu} /></Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
   )
 }
 
